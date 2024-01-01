c58691482ebcf566ea3dfb5f08cd5c89d5e29e18
arhamabeer <arhamabeerahmed@gmail.com>
2024-01-01 17:57:19 +0500

    feat: serving client server from backend server

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const app = express();
 
+const path = require("path");
+
 const PORT = process.env.PORT || 5010;
+
+app.use(express.static("public"));
+app.use(express.static("main.tsx"));
 
 app.get("/", (req, res) => {
   return res.json("Hello from Docker Server! 🐋 ");
+});
+
+app.get("/client", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "index.html"));
 });
 
 app.listen(PORT, () => {
