1bec5db07b7cd8b00ed94c865964696f970c551c
arhamabeer <arhamabeerahmed@gmail.com>
2024-01-01 16:09:03 +0500

    update: emoji in index.js

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const app = express();
 const PORT = process.env.PORT || 5010;
 
 app.get("/", (req, res) => {
-  return res.json("Hello from Docker Server!");
+  return res.json("Hello from Docker Server! 🐋 ");
 });
 
 app.listen(PORT, () => {
-  console.log("Docker Server is running...");
+  console.log("Docker Server is running... 🐬 ");
 });
